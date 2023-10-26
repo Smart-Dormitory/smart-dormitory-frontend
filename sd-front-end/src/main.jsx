c3754d523244cf1edfd45373362026954779070d
junoshon <sanoldfox@naver.com>
2023-10-27 02:32:57 +0900

    feat: react-rotuer 추가

--- sd-front-end/src/main.jsx
+++ sd-front-end/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import NotFound from "./pages/NotFound.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    
+    errorElement : <NotFound/>,
+    children:[
+      {
+        index:true, path:'/', element: <Home/>
+      },
+      {path: "/login" , element: <Login/>}
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
