59a3dfbca1276d6c3a8b90bc2c2750cb945b23ab
junoshon <sanoldfox@naver.com>
2023-10-27 10:59:56 +0900

    feat: Home.jsx에 Notice.jsx, Menu.jsx 추가

--- sd-front-end/src/pages/Home.jsx
+++ sd-front-end/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from "../components/Header.jsx";
 import Main from "../components/Main.jsx";
+import Notice from "../components/Notice.jsx";
+import Menu from "../components/Menu.jsx";
 
 export default function Home() {
     return (
         <div>
             <Header/>
-             <Main/>
+            <Main/>
+            <Notice/>
+            <Menu/>
         </div>
     )
 }
